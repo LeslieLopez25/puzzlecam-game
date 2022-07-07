a9c208a67d4f3c4a561548b3a7fad54fd2c0f270
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-06 22:46:08 -0500

    Added drag and drop function

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ let CONTEXT = null;
 let SCALER = 0.8;
 let SIZE = { x: 0, y: 0, width: 0, height: 0, rows: 3, columns: 3 };
 let PIECES = [];
+let SELECTED_PIECE = null;
 
 function main() {
   CANVAS = document.getElementById("myCanvas");
   CONTEXT = CANVAS.getContext("2d");
+  addEventListener();
 
   let promise = navigator.mediaDevices.getUserMedia({ video: true });
   promise
@@ -26,6 +28,78 @@ function main() {
     .catch(function (err) {
       alert("Camera error: " + err);
     });
+}
+
+function addEventListener() {
+  CANVAS.addEventListener("mousedown", onMouseDown);
+  CANVAS.addEventListener("mousemove", onMouseMove);
+  CANVAS.addEventListener("mouseup", onMouseUp);
+  CANVAS.addEventListener("touchstart", onTouchStart);
+  CANVAS.addEventListener("touchmove", onTouchMove);
+  CANVAS.addEventListener("touchend", onTouchEnd);
+}
+
+function onTouchStart(evt) {
+  let loc = {
+    x: evt.touches[0].clientX,
+    y: evt.touches[0].clientY,
+  };
+  onMouseDown(loc);
+}
+
+function onTouchMove(evt) {
+  let loc = {
+    x: evt.touches[0].clientX,
+    y: evt.touches[0].clientY,
+  };
+  onMouseMove(loc);
+}
+
+function onTouchEnd() {
+  onMouseUp();
+}
+
+function onMouseDown(evt) {
+  SELECTED_PIECE = getPressedPiece(evt);
+  if (SELECTED_PIECE != null) {
+    const index = PIECES.indexOf(SELECTED_PIECE);
+    if (index > -1) {
+      PIECES.splice(index, 1);
+      PIECES.push(SELECTED_PIECE);
+    }
+    SELECTED_PIECE.offset = {
+      x: evt.x - SELECTED_PIECE.x,
+      y: evt.y - SELECTED_PIECE.y,
+    };
+  }
+}
+
+function onMouseMove(evt) {
+  if (SELECTED_PIECE != null) {
+    SELECTED_PIECE.x = evt.x - SELECTED_PIECE.offset.x;
+    SELECTED_PIECE.y = evt.y - SELECTED_PIECE.offset.y;
+  }
+}
+
+function onMouseUp() {
+  if (SELECTED_PIECE.isClose()) {
+    SELECTED_PIECE.snap();
+  }
+  SELECTED_PIECE = null;
+}
+
+function getPressedPiece(loc) {
+  for (let i = PIECES.length - 1; i >= 0; i--) {
+    if (
+      loc.x > PIECES[i].x &&
+      loc.x < PIECES[i].x + PIECES[i].width &&
+      loc.y > PIECES[i].y &&
+      loc.y < PIECES[i].y + PIECES[i].height
+    ) {
+      return PIECES[i];
+    }
+  }
+  return null;
 }
 
 function handleResize() {
@@ -84,6 +158,12 @@ class Piece {
   constructor(rowIndex, colIndex) {
     this.rowIndex = rowIndex;
     this.colIndex = colIndex;
+    this.x = SIZE.x + (SIZE.width * this.colIndex) / SIZE.columns;
+    this.y = SIZE.y + (SIZE.height * this.rowIndex) / SIZE.rows;
+    this.width = SIZE.width / SIZE.columns;
+    this.height = SIZE.height / SIZE.rows;
+    this.xCorrect = this.x;
+    this.yCorrect = this.y;
   }
   draw(context) {
     context.beginPath();
@@ -103,4 +183,26 @@ class Piece {
     context.rect(this.x, this.y, this.width, this.height);
     context.stroke();
   }
+  isClose() {
+    if (
+      distance(
+        { x: this.x, y: this.y },
+        { x: this.xCorrect, y: this.yCorrect }
+      ) <
+      this.width / 3
+    ) {
+      return true;
+    }
+    return false;
+  }
+  snap() {
+    this.x = this.xCorrect;
+    this.y = this.yCorrect;
+  }
+}
+
+function distance(p1, p2) {
+  return Math.sqrt(
+    (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)
+  );
 }
