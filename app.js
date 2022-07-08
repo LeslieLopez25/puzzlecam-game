2b4b097fa57d26ae87ffc9d4195dd10fd8a39794
Leslie Lopez <leslie2515lopez@gmail.com>
2022-07-07 21:53:21 -0500

    Added gameplay function

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let SCALER = 0.8;
 let SIZE = { x: 0, y: 0, width: 0, height: 0, rows: 3, columns: 3 };
 let PIECES = [];
 let SELECTED_PIECE = null;
+let START_TIME = null;
+let END_TIME = null;
 
 function main() {
   CANVAS = document.getElementById("myCanvas");
@@ -22,12 +24,74 @@ function main() {
         handleResize();
         // window.addEventListener("resize", handleResize);
         initializePieces(SIZE.rows, SIZE.columns);
-        updateCanvas();
+        updateGame();
       };
     })
     .catch(function (err) {
       alert("Camera error: " + err);
     });
+}
+
+function setDifficulty() {
+  let diff = document.getElementById("difficulty").value;
+  switch (diff) {
+    case "easy":
+      initializePieces(3, 3);
+      break;
+    case "medium":
+      initializePieces(5, 5);
+      break;
+    case "hard":
+      initializePieces(10, 10);
+      break;
+    case "insane":
+      initializePieces(40, 25);
+      break;
+  }
+}
+
+function restart() {
+  START_TIME = new Data().getTime();
+  END_TIME = null;
+  randomizePieces();
+  document.getElementById("menuItems").style.display = "none";
+}
+
+function updateTime() {
+  let now = new Date().getTime();
+  if (START_TIME != null) {
+    if (END_TIME != null) {
+      document.getElementById("time").innerHTML = formatTime(
+        END_TIME - START_TIME
+      );
+    } else {
+      document.getElementById("time").innerHTML = formatTime(now - START_TIME);
+    }
+  }
+}
+
+function isComplete() {
+  for (let i = 0; i < PIECES.length; i++) {
+    if (PIECES[i].correct == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function formatTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let s = Math.floor(seconds % 60);
+  let m = Math.floor((seconds % (60 * 60)) / 60);
+  let h = Math.floor((seconds % (60 * 60 * 24)) / (60 * 60));
+
+  let formattedTime = h.toString().padStart(2, "0");
+  formattedTime += ":";
+  formattedTime += m.toString().padStart(2, "0");
+  formattedTime += ":";
+  formattedTime += s.toString().padStart(2, "0");
+
+  return formattedTime;
 }
 
 function addEventListener() {
@@ -71,6 +135,7 @@ function onMouseDown(evt) {
       x: evt.x - SELECTED_PIECE.x,
       y: evt.y - SELECTED_PIECE.y,
     };
+    SELECTED_PIECE.correct = false;
   }
 }
 
@@ -84,6 +149,10 @@ function onMouseMove(evt) {
 function onMouseUp() {
   if (SELECTED_PIECE.isClose()) {
     SELECTED_PIECE.snap();
+    if (isComplete() && END_TIME == null) {
+      let now = new Date().getTime();
+      END_TIME = now;
+    }
   }
   SELECTED_PIECE = null;
 }
@@ -118,7 +187,7 @@ function handleResize() {
   SIZE.y = window.innerHeight / 2 - SIZE.height / 2;
 }
 
-function updateCanvas() {
+function updateGame() {
   CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
 
   CONTEXT.globalAlpha = 0.5;
@@ -128,7 +197,8 @@ function updateCanvas() {
   for (let i = 0; i < PIECES.length; i++) {
     PIECES[i].draw(CONTEXT);
   }
-  window.requestAnimationFrame(updateCanvas);
+  updateTime();
+  window.requestAnimationFrame(updateGame);
 }
 
 function initializePieces(rows, cols) {
@@ -151,6 +221,7 @@ function randomizePieces() {
     };
     PIECES[i].x = loc.x;
     PIECES[i].y = loc.y;
+    PIECES[i].correct = false;
   }
 }
 
@@ -164,6 +235,7 @@ class Piece {
     this.height = SIZE.height / SIZE.rows;
     this.xCorrect = this.x;
     this.yCorrect = this.y;
+    this.correct = true;
   }
   draw(context) {
     context.beginPath();
@@ -198,6 +270,7 @@ class Piece {
   snap() {
     this.x = this.xCorrect;
     this.y = this.yCorrect;
+    this.correct = true;
   }
 }
 
